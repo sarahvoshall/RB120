05f1f5d7ff939674e283c0529893503b025fe291
Sarah Voshal <sarahvoshall@gmail.com>
2021-09-09 14:09:07 -0400

    third

---
+++ RB120_Sublime/new_file.rb
@@ -0,0 +1,21 @@
+module Speak
+	def speak(sound)
+		puts sound
+	end 
+end
+
+class GoodDog
+	include Speak 
+end 
+
+class HumanBeing
+	include Speak
+end
+
+sparky = GoodDog.new
+bob = HumanBeing.new 
+
+sparky.speak('Arf!')
+bob.speak('Hello!')
+
+puts GoodDog.ancestors
